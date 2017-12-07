cb56749dbf3126b8bdb25503061bfd9afe549601
Matt Silveira <msilveira@tnc.org>
2017-12-07 14:54:31 -0700

    add the click and selection functionality on map for wetlands and survey points

--- js/clicks.js
+++ js/clicks.js
@@ -6,6 +6,8 @@ function ( declare, Query, QueryTask ) {
 
         return declare(null, {
 			eventListeners: function(t){
+				// call the map click function at the start to load it
+				t.clicks.mapClickFunction(t);
 				// on zoom end turn on layer with and without borders depending on a zoom level scale of 75000 ///////////////
 				t.map.on("zoom-end", function(){
 					if(t.map.getScale() < 75001){
@@ -13,13 +15,11 @@ function ( declare, Query, QueryTask ) {
 					}else{
 						t.obj.scale = 'out'
 					}
-					// t.clicks.toggleFunc(t)
 				})
 				// Main header toggle button///////////////////////////////////////////
 				$('#' + t.id + 'mainRadioBtns input').on('click',function(c){
 					// console.log(c);
 					let val = c.currentTarget.value;
-					console.log(val)
 					t.currentCheckVal = c.currentTarget;
 					const sections = $(".aoc-contentBelowHeader .aoc-mainSection");
 					t.clicks.toggleFunc(t)
@@ -27,10 +27,8 @@ function ( declare, Query, QueryTask ) {
 				// checkboxes for suplementary data
 				$('#' + t.id + 'supDataWrapper input').on('click',function(c){
 					let val = parseInt(c.currentTarget.value.split('-')[1]);
-					console.log(val)
 					if(c.currentTarget.checked){
 						t.obj.visibleLayers.push(val)
-						console.log(t.obj.visibleLayers)
 					}else{
 						// remove item from visible layer list
 						let index = t.obj.visibleLayers.indexOf(val)
@@ -40,6 +38,122 @@ function ( declare, Query, QueryTask ) {
 					t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
 				});
 			},
+
+			// map click functionality call the map click query function //////////////////////////////////////////////////
+			mapClickFunction: function(t){
+				t.map.on('click',function(c){
+					t.obj.pnt = c.mapPoint;
+					t.clicks.mapClickQuery(t,t.obj.pnt); // call t.mapClickQuery function
+				});
+			},
+			// map click query function /////////////////////////////////////////////////////////////////////
+			mapClickQuery: function(t, p){
+				t.layerDefinitions = [];
+				// wetland click query /////////////////////////////////////////////////////////////////
+				let wetlandIndex = t.obj.visibleLayers.indexOf(t.wetlands);
+				if (wetlandIndex > -1){
+					t.qw = new Query();
+					t.qtw = new QueryTask(t.url + "/" + t.wetlandsSel);
+					t.qw.geometry = p;
+					t.qw.returnGeometry = true;
+					t.qw.outFields = ["*"];
+					// query the map on click
+					t.qtw.execute(t.qw, function(evt){
+						if (evt.features.length > 0) {
+							// push wetland selected layer to visible layers array
+							if(t.obj.visibleLayers.indexOf(t.wetlandsSel) > -1){
+								'do nothing'
+							}else{
+								t.obj.visibleLayers.push(t.wetlandsSel);
+								// t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
+							}
+							
+							// t.obj.visibleLayers.push(t.wetlandsSel);
+							// create the fish where clause to be used for the survey point seleceted feature
+							t.obj.wetlandOID = evt.features[0].attributes.OBJECTID;
+							t.obj.wetlandWhere = "OBJECTID = " + t.obj.wetlandOID;
+							// set layer deffs
+							t.layerDefinitions[t.wetlandsSel] = t.obj.wetlandWhere;
+							t.dynamicLayer.setLayerDefinitions(t.layerDefinitions);
+						}else{
+							// remove the selected layer if no features were clicked
+							let index = t.obj.visibleLayers.indexOf(t.wetlandsSel)
+							if (index > -1) {
+								t.obj.visibleLayers.splice(index,1);
+							}
+						}
+						t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
+					});
+				}
+
+				
+				// // prw wetland click query //////////////////////////////////////////
+				// let prwIndex = t.obj.visibleLayers.indexOf(t.prwWetlands);
+				// if (prwIndex > -1) {
+				// 	t.qp = new Query();
+				// 	t.qtp = new QueryTask(t.url + "/" + t.prwWetlands);
+				// 	t.qp.geometry = p;
+				// 	t.qp.returnGeometry = true;
+				// 	t.qp.outFields = ["*"];
+				// 	// query the map on click
+				// 	t.qtp.execute(t.qp, function(evt){
+				// 		if (evt.features.length > 0) {
+				// 			console.log('prw click')
+				// 		}
+
+				// 	});
+				// }
+				
+
+				// survey rank point click /////////////////////////////////////////////////////////////////////
+				let surveyRankIndex = t.obj.visibleLayers.indexOf(t.surveyRank);
+				if (surveyRankIndex > 0){
+					// adjust the tolerance of the point click
+					var centerPoint = new esri.geometry.Point(p.x,p.y,p.spatialReference);
+					var mapWidth = t.map.extent.getWidth();
+					var mapWidthPixels = t.map.width;
+					var pixelWidth = mapWidth/mapWidthPixels;
+					// change the tolerence below to adjust how many pixels will be grabbed when clicking on a point or line
+					var tolerance = 10 * pixelWidth;
+					var pnt = p;
+					var ext = new esri.geometry.Extent(1,1, tolerance, tolerance, p.spatialReference);
+					// query for the fish passage points click ///////////////////////////////
+					t.q2 = new Query();
+					t.qt2 = new QueryTask(t.url + "/" + t.surveyRank);
+					t.q2.geometry = ext.centerAt(centerPoint);;
+					t.q2.returnGeometry = true;
+					t.q2.outFields = ["*"];
+					// query the map on click
+					t.qt2.execute(t.q2, function(evt){
+						if (evt.features.length > 0){
+							// test to see if the survey point selected is already visible
+							if(t.obj.visibleLayers.indexOf(t.surveyRankSel) > 0){
+								'do nothing'
+							}else{
+								t.obj.visibleLayers.push(t.surveyRankSel);
+								t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
+							}
+							// create the fish where clause to be used for the survey point seleceted feature
+							t.obj.fishOID = evt.features[0].attributes.OBJECTID;
+							t.obj.fishWhere = "OBJECTID = " + t.obj.fishOID;
+							
+							// set layer deffs
+							
+							t.layerDefinitions[t.surveyRankSel] = t.obj.fishWhere;
+							t.dynamicLayer.setLayerDefinitions(t.layerDefinitions);
+						}else{
+							// remove the selected layer if no features were clicked
+							let index = t.obj.visibleLayers.indexOf(t.surveyRankSel)
+							if (index > -1) {
+								t.obj.visibleLayers.splice(index,1);
+							}
+						}
+						t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
+					});
+				}
+				// t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
+			},
+
 			// main toggle button function./////////////////////////////////////////////
 			toggleFunc: function(t){
 				// declare layers for each section
@@ -72,11 +186,21 @@ function ( declare, Query, QueryTask ) {
 							t.obj.visibleLayers = t.obj.visibleLayers.filter(function(x){
 								return watershedContr.indexOf(x) < 0;
 							})
+							let waterIndex = t.obj.visibleLayers.indexOf(t.wetlandsSel);
+							if (waterIndex > -1) {
+								t.obj.visibleLayers.splice(waterIndex,1);
+							}
 							break;
 						case 'fish':
+							// remove the fish passage layers if checkboxes is unchecked
 							t.obj.visibleLayers = t.obj.visibleLayers.filter(function(x){
 								return fishPassage.indexOf(x) < 0;
 							})
+							// remove the selected layer if its there
+							let index = t.obj.visibleLayers.indexOf(t.surveyRankSel);
+							if (index > -1) {
+								t.obj.visibleLayers.splice(index,1);
+							}
 							break;
 						default:
 							console.log('none of the cases matched');
@@ -86,17 +210,21 @@ function ( declare, Query, QueryTask ) {
 				t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
 			},
 			makeVariables: function(t){
-				t.aoc = 0;
-				t.lowerFoxBound = 1;
-				t.countyBounds = 2;
-				t.surveyRank = 3;
-				t.otherSurvey = 4;
-				t.habitatSites = 5;
-				t.wetlands = 6;
-				t.prwWetlands = 7;
-				t.wetlandsBord = 8;
-				t.prwWetlandsBord = 9;
-				t.kepBound = 10;
+				t.aoc = 3;
+				t.lowerFoxBound = 4;
+				t.countyBounds = 5;
+				t.surveyRank = 6;
+				t.otherSurvey = 7;
+				t.habitatSites = 8;
+				t.wetlands = 9;
+				t.prwWetlands = 10;
+				t.wetlandsBord = 11;
+				t.prwWetlandsBord = 12;
+				t.kepBound = 13;
+
+				t.surveyRankSel = 2;
+				t.wetlandsSel= 1;
+				t.habitatSel = 0;
 			},
 			commaSeparateNumber: function(val){
 				while (/(\d+)(\d{3})/.test(val.toString())){
