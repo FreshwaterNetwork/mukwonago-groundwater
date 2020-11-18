606c831ca06ae7077e8becdf4f3fd787ab5a8b67
matts5517 <msilveira@tnc.org>
2020-11-18 12:45:56 -0700

    build out click on raster for pixel value

--- js/esriapi.js
+++ js/esriapi.js
@@ -4,6 +4,8 @@ define([
   "esri/SpatialReference",
   "esri/tasks/query",
   "esri/tasks/QueryTask",
+  "esri/tasks/IdentifyTask",
+  "esri/tasks/IdentifyParameters",
   "dojo/_base/declare",
   "esri/layers/FeatureLayer",
   "esri/symbols/SimpleLineSymbol",
@@ -18,6 +20,8 @@ define([
   SpatialReference,
   Query,
   QueryTask,
+  IdentifyTask,
+  IdentifyParameters,
   declare,
   FeatureLayer,
   SimpleLineSymbol,
@@ -40,11 +44,18 @@ define([
         t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
       }
       t.dynamicLayer.on("load", function () {
+        const startingExtent = new Extent(
+          -9875817.047046928,
+          5276075.334701181,
+          -9811304.195174327,
+          5311236.367712314,
+          new SpatialReference({ wkid: 102100 })
+        );
         // User selections on chosen menus
-        $("#" + t.id + "ch-ISL")
-          .chosen({ width: "182px", disable_search: true })
-          .change(t, function (c, p) {});
-        t.obj.opacityVal = 50;
+        // $("#" + t.id + "ch-ISL")
+        //   .chosen({ width: "182px", disable_search: true })
+        //   .change(t, function (c, p) {});
+        t.obj.opacityVal = 25;
         // work with Opacity sliders /////////////////////////////////////////////
         $("#" + t.id + "sldr").slider({
           min: 0,
@@ -57,64 +68,43 @@ define([
           t.obj.opacityVal = 1 - ui.value / 100;
           t.dynamicLayer.setOpacity(t.obj.opacityVal);
         });
-        // hide the framework toolbox
-        $("#map-utils-control").hide();
+
         // create layers array
         t.layersArray = t.dynamicLayer.layerInfos;
+
+        // if not state set, set extent to starting extent
         if (t.obj.stateSet == "no") {
-          t.map.setExtent(t.dynamicLayer.fullExtent.expand(0.25), true);
+          t.map.setExtent(startingExtent.expand(0.75), true);
         }
+
         ////////////////////////////// save and share code below ////////////////////////////////////////////////////////////
         if (t.obj.stateSet == "yes") {
-          // // bring in layer defs var
-          // t.obj.layerDefinitions = [];
-          // // check the correct cb's
-          // $.each(t.obj.cbTracker, function(i,v){
-          // 	$('#' + v).prop('checked', true);
-          // })
-          // // put the radio button in the right place
-          // $('#' + t.obj.radButtonTracker).prop('checked', true);
-          // // remove all blueFont classes first before adding it back to current target
-          // $.each($('#' + t.id + 'contentWrapper').find('.aoc-mainCB'), function(i,v){
-          // 	$(v).removeClass('blueFont');
-          // })
-          // $('#' + t.obj.radButtonTracker).parent().prev().addClass('blueFont')
-          // // if there is a selected layer set layer defs and add layer to map
-          // t.obj.layerDefinitions[t.obj.queryTracker] = t.obj.query;
-          // t.dynamicLayer.setLayerDefinitions(t.obj.layerDefinitions);
-          // // if something has been selected slide down the correct att box and populate
-          // if(t.obj.query){
-          // 	$('#' + t.id + t.obj.toggleTracker + "Wrapper").slideDown();
-          // 	$('#' + t.id + "selectedAttributes").show();
-          // 	let v1 = $($('#' + t.id + t.obj.toggleTracker + "Wrapper").find('.aoc-attText')[0]).html(t.obj.attsTracker[0]);
-          // 	let v2 = $($('#' + t.id + t.obj.toggleTracker + "Wrapper").find('.aoc-attText')[1]).html(t.obj.attsTracker[1])
-          // 	// handle the report link for habitat
-          // 	if(t.obj.toggleTracker == 'habitat'){
-          // 		var html = "<a style='color:blue;' href='plugins/AOCapp/assets/report.pdf#page=" + t.obj.attsTracker[2] + "' target='_blank'>Click to view in report</a>"
-          // 		let v3 = $($('#' + t.id + t.obj.toggleTracker + "Wrapper").find('.aoc-attText')[2]).html(html)
-          // 	}else{
-          // 		let v3 = $($('#' + t.id + t.obj.toggleTracker + "Wrapper").find('.aoc-attText')[2]).html(t.obj.attsTracker[2])
-          // 	}
-          // 	let v4 = $($('#' + t.id + t.obj.toggleTracker + "Wrapper").find('.aoc-attText')[3]).html(t.obj.attsTracker[3])
-          // 	let v5 = $($('#' + t.id + t.obj.toggleTracker + "Wrapper").find('.aoc-attText')[4]).html(t.obj.attsTracker[4])
-          // 	let v6 = $($('#' + t.id + t.obj.toggleTracker + "Wrapper").find('.aoc-attText')[5]).html(t.obj.attsTracker[5])
-          // 	let v7 = $($('#' + t.id + t.obj.toggleTracker + "Wrapper").find('.aoc-attText')[6]).html(t.obj.attsTracker[6])
-          // 	let v8 = $($('#' + t.id + t.obj.toggleTracker + "Wrapper").find('.aoc-attText')[7]).html(t.obj.attsTracker[7])
-          // }
           // display the correct layers on the map
           t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
-          // check the correct checkboxes in the sup data section ////////////////////////
-          // $.each(t.obj.supCheckArray,function(i,y){
-          // 	$.each($('#' + t.id + 'supData input'),function(i,v){
-          // 		if(y == v.value){
-          // 			$(v).prop('checked', 'true');
-          // 		}
-          // 	})
-          // })
+
           // zoom to the correct area of the map
           t.map.setExtent(t.obj.extent.expand(1.5), true);
         }
       });
+      t.map.on("click", (event) => {
+        getRasterValue(event.mapPoint, 10);
+      });
+      function getRasterValue(mapPoint, layerID) {
+        const identifyTask = new IdentifyTask(t.obj.url);
+        const identifyParams = new IdentifyParameters();
+        identifyParams.tolerance = 3;
+        identifyParams.returnGeometry = true;
+        identifyParams.layerIds = [layerID];
+        identifyParams.geometry = mapPoint;
+        identifyParams.mapExtent = t.map.extent;
+        identifyTask.execute(identifyParams).addCallback(function (response) {
+          // console.log(response[0].feature);
+          displayRasterValue(response[0].feature);
+        });
+      }
+      function displayRasterValue(value) {
+        console.log(value.attributes);
+      }
     },
   });
 });
