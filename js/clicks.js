af2e789f1c3b6bb6196de6d9e25f936216e05d40
Matt Silveira <msilveira@tnc.org>
2021-01-07 17:21:35 -0700

    updates to muk app

--- js/clicks.js
+++ js/clicks.js
@@ -1,63 +1,81 @@
 define([
-	"dojo/_base/declare",
-	"esri/tasks/query",
-	"esri/tasks/QueryTask",
+    "dojo/_base/declare",
+    "esri/tasks/query",
+    "esri/tasks/QueryTask",
 ], function (declare, Query, QueryTask) {
-	"use strict";
+    "use strict";
 
-	return declare(null, {
-		eventListeners: function (t) {
-			// on agree checkbox click
-			$("#" + t.id + "agree-option").on("click", (evt) => {
-				if (evt.currentTarget.checked) {
-					// enable agree button
-					$(".mgw-agree-button").prop("disabled", false);
-				} else {
-					// disable agree button
-					$(".mgw-agree-button").prop("disabled", true);
-				}
-			});
-			// on agree button click
-			$(".mgw-agree-button").on("click", (evt) => {
-				// Create ESRI objects and event listeners
-				t.esriapi.esriApiFunctions(t);
-				// hide the agreement box
-				$(".mgw-agree-wrapper").hide();
-				// slide down the main app wrapper
-				$(".mgw-main-app-wrapper").slideDown();
-			});
-		},
+    return declare(null, {
+        eventListeners: function (t) {
+            // on agree checkbox click
+            $("#" + t.id + "agree-option").on("click", (evt) => {
+                if (evt.currentTarget.checked) {
+                    // enable agree button
+                    $(".mgw-agree-button").prop("disabled", false);
+                } else {
+                    // disable agree button
+                    $(".mgw-agree-button").prop("disabled", true);
+                }
+            });
+            // on agree button click
+            $(".mgw-agree-button").on("click", (evt) => {
+                // Create ESRI objects and event listeners
+                t.esriapi.esriApiFunctions(t);
+                // hide the agreement box
+                $(".mgw-agree-wrapper").hide();
+                // slide down the main app wrapper
+                $(".mgw-main-app-intro-wrapper").slideDown();
+            });
 
-		// map click functionality call the map click query function //////////////////////////////////////////////////
-		mapClickFunction: function (t) {
-			t.map.on("click", function (c) {
-				t.obj.pnt = c.mapPoint;
-				t.clicks.mapClickQuery(t, t.obj.pnt); // call t.mapClickQuery function
-			});
-		},
-		// map click query function /////////////////////////////////////////////////////////////////////
-		mapClickQuery: function (t, p) {
-			console.log(p);
-		},
+            // on known location button click
+            $(".mgw-known-location-button").on("click", (evt) => {
+                console.log("known location");
+                ("mgw-known-location-wrapper");
+                $(".mgw-known-location-wrapper").show();
+                $(".mgw-main-app-intro-wrapper").hide();
+                // // Create ESRI objects and event listeners
+                // t.esriapi.esriApiFunctions(t);
+                // // hide the agreement box
+                // $(".mgw-agree-wrapper").hide();
+                // // slide down the main app wrapper
+                // $(".mgw-main-app-intro-wrapper").slideDown();
+            });
+            // on search location button click
+            $(".mgw-search-location-button").on("click", (evt) => {
+                console.log("search location");
+            });
+        },
 
-		makeVariables: function (t) {},
-		commaSeparateNumber: function (val) {
-			while (/(\d+)(\d{3})/.test(val.toString())) {
-				val = val.toString().replace(/(\d+)(\d{3})/, "$1" + "," + "$2");
-			}
-			return val;
-		},
-		abbreviateNumber: function (num) {
-			if (num >= 1000000000) {
-				return (num / 1000000000).toFixed(1).replace(/\.0$/, "") + "B";
-			}
-			if (num >= 1000000) {
-				return (num / 1000000).toFixed(1).replace(/\.0$/, "") + "M";
-			}
-			if (num >= 1000) {
-				return (num / 1000).toFixed(1).replace(/\.0$/, "") + "K";
-			}
-			return num;
-		},
-	});
+        // map click functionality call the map click query function //////////////////////////////////////////////////
+        mapClickFunction: function (t) {
+            t.map.on("click", function (c) {
+                t.obj.pnt = c.mapPoint;
+                t.clicks.mapClickQuery(t, t.obj.pnt); // call t.mapClickQuery function
+            });
+        },
+        // map click query function /////////////////////////////////////////////////////////////////////
+        mapClickQuery: function (t, p) {
+            console.log(p);
+        },
+
+        makeVariables: function (t) {},
+        commaSeparateNumber: function (val) {
+            while (/(\d+)(\d{3})/.test(val.toString())) {
+                val = val.toString().replace(/(\d+)(\d{3})/, "$1" + "," + "$2");
+            }
+            return val;
+        },
+        abbreviateNumber: function (num) {
+            if (num >= 1000000000) {
+                return (num / 1000000000).toFixed(1).replace(/\.0$/, "") + "B";
+            }
+            if (num >= 1000000) {
+                return (num / 1000000).toFixed(1).replace(/\.0$/, "") + "M";
+            }
+            if (num >= 1000) {
+                return (num / 1000).toFixed(1).replace(/\.0$/, "") + "K";
+            }
+            return num;
+        },
+    });
 });
--- js/esriapi.js
+++ js/esriapi.js
@@ -1,89 +1,148 @@
 define([
-  "esri/layers/ArcGISDynamicMapServiceLayer",
-  "esri/geometry/Extent",
-  "esri/SpatialReference",
-  "esri/tasks/query",
-  "esri/tasks/QueryTask",
-  "dojo/_base/declare",
-  "esri/layers/FeatureLayer",
-  "esri/symbols/SimpleLineSymbol",
-  "esri/symbols/SimpleFillSymbol",
-  "esri/symbols/SimpleMarkerSymbol",
-  "esri/graphic",
-  "dojo/_base/Color",
-  "esri/layers/GraphicsLayer",
+    "esri/layers/ArcGISDynamicMapServiceLayer",
+    "esri/geometry/Extent",
+    "esri/SpatialReference",
+    "esri/tasks/query",
+    "esri/tasks/QueryTask",
+    "dojo/_base/declare",
+    "esri/layers/FeatureLayer",
+    "esri/symbols/SimpleLineSymbol",
+    "esri/symbols/SimpleFillSymbol",
+    "esri/symbols/SimpleMarkerSymbol",
+    "esri/geometry/Circle",
+    "esri/dijit/Search",
+    "esri/graphic",
+    "dojo/_base/Color",
+    "esri/layers/GraphicsLayer",
 ], function (
-  ArcGISDynamicMapServiceLayer,
-  Extent,
-  SpatialReference,
-  Query,
-  QueryTask,
-  declare,
-  FeatureLayer,
-  SimpleLineSymbol,
-  SimpleFillSymbol,
-  SimpleMarkerSymbol,
-  Graphic,
-  Color,
-  GraphicsLayer
+    ArcGISDynamicMapServiceLayer,
+    Extent,
+    SpatialReference,
+    Query,
+    QueryTask,
+    declare,
+    FeatureLayer,
+    SimpleLineSymbol,
+    SimpleFillSymbol,
+    SimpleMarkerSymbol,
+    Circle,
+    Search,
+    Graphic,
+    Color,
+    GraphicsLayer
 ) {
-  "use strict";
+    "use strict";
 
-  return declare(null, {
-    esriApiFunctions: function (t) {
-      // Add dynamic map service
-      t.dynamicLayer = new ArcGISDynamicMapServiceLayer(t.obj.url, {
-        opacity: 0.9,
-      });
-      t.map.addLayer(t.dynamicLayer);
-      if (t.obj.visibleLayers.length > 0) {
-        t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
-      }
-      t.dynamicLayer.on("load", function () {
-        t.obj.opacityVal = 25;
-        // work with Opacity sliders /////////////////////////////////////////////
-        $("#" + t.id + "sldr").slider({
-          min: 0,
-          max: 100,
-          range: false,
-          values: [t.obj.opacityVal],
-        });
+    return declare(null, {
+        esriApiFunctions: function (t) {
+            // Add dynamic map service
+            t.dynamicLayer = new ArcGISDynamicMapServiceLayer(t.obj.url, {
+                opacity: 0.9,
+            });
+            t.map.addLayer(t.dynamicLayer);
+            if (t.obj.visibleLayers.length > 0) {
+                t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
+            }
+            console.log("search test");
+            t.search = new Search(
+                {
+                    map: t.map,
+                    enableInfoWindow: false,
+                    enableHighlight: false,
+                    zoomScale: 2,
+                },
+                t.id + "mgw-search-bar"
+            );
+            console.log(t.search);
+            t.search.startup();
+            t.search.on("search-results", function (e) {
+                console.log(e, "eeeeee");
+            });
+            var circleSymb = new SimpleFillSymbol(
+                SimpleFillSymbol.STYLE_NULL,
+                new SimpleLineSymbol(
+                    SimpleLineSymbol.STYLE_SHORTDASHDOTDOT,
+                    new Color([105, 105, 105]),
+                    2
+                ),
+                new Color([255, 255, 0, 0.25])
+            );
+            t.map.on("click", (evt) => {
+                console.log(evt);
+                var circle = new Circle({
+                    center: evt.mapPoint,
+                    geodesic: true,
+                    radius: 3,
+                    radiusUnit: "esriMiles",
+                });
+                t.map.graphics.clear();
+                var graphic = new Graphic(circle, circleSymb);
+                t.map.graphics.add(graphic);
+                var query = new Query();
+                var qt = new QueryTask(
+                    "https://cirrus.tnc.org/arcgis/rest/services/FN_Wisconsin/mukwonago_groundwater_v11162020/MapServer/1"
+                );
+                query.geometry = circle.getExtent();
+                query.returnGeometry = true;
+                query.outFields = ["*"];
 
-        // t.dynamicLayer.setOpacity(1 - t.obj.opacityVal / 100); // set init opacity
-        $("#" + t.id + "sldr").on("slide", function (c, ui) {
-          t.obj.opacityVal = 1 - ui.value / 100;
-          // t.dynamicLayer.setOpacity(t.obj.opacityVal);
-        });
-        // hide the framework toolbox
-        // create layers array
-        t.layersArray = t.dynamicLayer.layerInfos;
-        if (t.obj.stateSet == "no") {
-          t.map.setExtent(t.dynamicLayer.fullExtent.expand(0.55), true);
-        }
+                qt.execute(query, function (evt) {
+                    console.log(evt);
+                    // if (evt.features.length > 0) {
+                    //     t.obj.maskClick = "yes";
+                    // } else {
+                    //     t.obj.maskClick = "no";
+                    // }
+                });
+            });
+            t.dynamicLayer.on("load", function () {
+                t.obj.opacityVal = 25;
+                // work with Opacity sliders /////////////////////////////////////////////
+                $("#" + t.id + "sldr").slider({
+                    min: 0,
+                    max: 100,
+                    range: false,
+                    values: [t.obj.opacityVal],
+                });
 
-        t.dynamicLayer.setOpacity(1 - t.obj.opacityVal / 100); // set init opacity
-        $("#" + t.id + "sldr").on("slide", function (c, ui) {
-          t.obj.opacityVal = 1 - ui.value / 100;
-          t.dynamicLayer.setOpacity(t.obj.opacityVal);
-        });
+                // t.dynamicLayer.setOpacity(1 - t.obj.opacityVal / 100); // set init opacity
+                $("#" + t.id + "sldr").on("slide", function (c, ui) {
+                    t.obj.opacityVal = 1 - ui.value / 100;
+                    // t.dynamicLayer.setOpacity(t.obj.opacityVal);
+                });
+                // hide the framework toolbox
+                // create layers array
+                t.layersArray = t.dynamicLayer.layerInfos;
+                if (t.obj.stateSet == "no") {
+                    t.map.setExtent(
+                        t.dynamicLayer.fullExtent.expand(0.55),
+                        true
+                    );
+                }
 
-        // create layers array
-        t.layersArray = t.dynamicLayer.layerInfos;
+                t.dynamicLayer.setOpacity(1 - t.obj.opacityVal / 100); // set init opacity
+                $("#" + t.id + "sldr").on("slide", function (c, ui) {
+                    t.obj.opacityVal = 1 - ui.value / 100;
+                    t.dynamicLayer.setOpacity(t.obj.opacityVal);
+                });
 
-        // if not state set, set extent to starting extent
-        if (t.obj.stateSet == "no") {
-          t.map.setExtent(startingExtent.expand(0.75), true);
-        }
+                // create layers array
+                t.layersArray = t.dynamicLayer.layerInfos;
 
-        ////////////////////////////// save and share code below ////////////////////////////////////////////////////////////
-        if (t.obj.stateSet == "yes") {
-          // display the correct layers on the map
-          t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
+                // if not state set, set extent to starting extent
+                if (t.obj.stateSet == "no") {
+                    t.map.setExtent(startingExtent.expand(0.75), true);
+                }
 
-          // zoom to the correct area of the map
-          t.map.setExtent(t.obj.extent.expand(1.5), true);
-        }
-      });
-    },
-  });
+                ////////////////////////////// save and share code below ////////////////////////////////////////////////////////////
+                if (t.obj.stateSet == "yes") {
+                    // display the correct layers on the map
+                    t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
+
+                    // zoom to the correct area of the map
+                    t.map.setExtent(t.obj.extent.expand(1.5), true);
+                }
+            });
+        },
+    });
 });
